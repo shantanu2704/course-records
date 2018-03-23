cb3af93c0ce0a106807c93136ff1cbe17a3d7564
Shantanu <shantanu2846@gmail.com>
2018-03-23 13:08:29 +0530

    Add a class to process the slack input

--- modules/slack-input/class-slack-input.php
+++ modules/slack-input/class-slack-input.php
@@ -47,10 +47,31 @@ if ( !class_exists( 'Slack_Input' ) ) {
 		 * @since 0.0.1
 		 */
 		function form_page_content() {
-			?>
-				<label for="json_input"><?php _e( 'JSON Input' ); ?></label>
-				<input type="text" name="json_input" id="json_input" value="<?php echo "Hello" ?>" class="regular-text" />
 
+			$dir		 = wp_upload_dir();
+			$contents	 = opendir( $dir[ 'basedir' ] );
+			$files		 = array();
+
+			while ( false !== ($entry = readdir( $contents ) ) ) {
+
+				$file_path = $dir[ 'basedir' ] . '/' . $entry;
+
+				if ( is_file( $file_path ) ) {
+					array_push( $files, $entry );
+				}
+			}
+
+			?>
+			<div class="wrap">
+				<form action="" method="POST">
+					<label for="cr_json_files">Select JSON file</label>
+					<select name="cr_json_files" id="cr_json_files">
+					<?php foreach ( $files as $file ) : ?>
+						<option value="<?php echo esc_attr( $file ); ?>" ><?php echo esc_html( $file ); ?></option>
+					<?php endforeach; ?>
+					</select>
+				</form>
+			</div>
 			<?php
 		}
 
