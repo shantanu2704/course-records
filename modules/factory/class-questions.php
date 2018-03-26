0c5dac1de95185ea92fdbfd9c3ceec58f411e72b
Shantanu <shantanu2846@gmail.com>
2018-03-26 17:51:56 +0530

    Add add_question method to the Questions class

--- modules/factory/class-questions.php
+++ modules/factory/class-questions.php
@@ -1,1 +1,48 @@
 <?php
+
+/**
+ * Course Records
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) )	exit();
+
+if ( !class_exists( 'Questions' ) ) {
+
+	/**
+	 * Post Factory
+	 *
+	 * @since 0.0.1
+	 */
+	class Questions {
+
+		private $content;
+		private $post_id;
+
+		/**
+		 * Constructor
+		 * @param array $json_input Decoded JSON input array for a question
+		 */
+		public function __construct( $json_input ) {
+			$this->content = $json_input;
+		}
+
+		public function add_question() {
+			// Create post object
+			$my_post = array(
+				'post_author'	 => $this->content[ 'name' ],
+				'post_date'		 => substr( $this->content[ 'datetime' ], 0, 10 ),
+				'post_content'	 => $this->content[ 'text' ],
+				'post_status'	 => 'publish',
+				'post_type'		 => 'question',
+				'comment_status' => 'open',
+			);
+			// Insert post into database
+			return wp_insert_post( $my_post );
+		}
+
+	}
+
+}
