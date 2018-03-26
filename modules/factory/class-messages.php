4e0ead5e0b36333917771780974f4611002b017f
Shantanu <shantanu2846@gmail.com>
2018-03-26 15:58:46 +0530

    Add constructor to class-messages

--- modules/factory/class-messages.php
+++ modules/factory/class-messages.php
@@ -1,2 +1,39 @@
 <?php
 
+/**
+ * Course Records
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit();
+
+if ( !class_exists( 'Messages' ) ) {
+
+	/**
+	 * Post Factory
+	 *
+	 * @since 0.0.1
+	 */
+	class Messages {
+		
+		private $content;
+		
+		private $thread;
+		
+		private $broadcast;
+		
+		/**
+		 * Constructor
+		 * @param array $json_input Decoded JSON input array for a message
+		 * @param bool $is_thread Is the current message part of a thread
+		 * @param bool $was_broadcasted Was the current message broadcasted
+		 */
+		public function __construct( $json_input, $is_thread = FALSE, $was_broadcasted = FALSE ) {
+			$this->content = $json_input;
+			$this->thread = $is_thread;
+			$this->broadcast = $was_broadcasted;
+		}
+	}
+}
