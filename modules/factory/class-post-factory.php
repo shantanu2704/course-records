00662b3d6731dea51f79d6f1a0b3d8db477e26f3
Shantanu <shantanu2846@gmail.com>
2018-03-26 15:05:14 +0530

    Update documentation

--- modules/factory/class-post-factory.php
+++ modules/factory/class-post-factory.php
@@ -22,29 +22,50 @@ if ( !class_exists( 'Post_Factory' ) ) {
 
 		/**
 		 * Constructor
+		 * @since 0.0.1
 		 */
 		public function __construct( $json_input ) {
 			$this->json_content = $json_input;
 			$this->instantiate_classes( $this->json_content );
 		}
 
+		/**
+		 * Instantiate the appropriate class
+		 * @param array $content JSON of a message converted to an array
+		 * @since 0.0.1
+		 */
 		private function instantiate_classes( $content ) {
+			// Check if the current element is a task
 			if ( $this->is_task() ) {
 				$task = new Task( $content );
-			}elseif ( $this->is_question() ) {
+			}// Or if it is a question
+			elseif ( $this->is_question() ) {
 				$task = new Question( $content );
-			}elseif ( $this->is_thread_message() ) {
+			}// Or if it is a message in a thread
+			elseif ( $this->is_thread_message() ) {
 				$message = new Message( $content, true, true );
-			}else {
+			}// Or is just a regular message
+			else {
 				$message = new Message( $content );
 			}
 		}
 
+		/**
+		 * Check if the current message is part of a thread
+		 * @param array $content
+		 * @return bool
+		 * @since 0.0.1
+		 */
 		private function is_thread_message( $content ) {
 			$is_thread_message = ( array_key_exists( 'thread_ts', $content ) ) ? TRUE : FALSE;
 			return $is_thread_message;
 		}
 
+		/**
+		 * Check if the current message is a task
+		 * @param array $content
+		 * @return bool
+		 */
 		private function is_task( $content ) {
 			if ( isset( $content[ 'text' ] ) ) {
 				$query	 = '<@U9DQ94KM3> <!channel>';
@@ -53,6 +74,12 @@ if ( !class_exists( 'Post_Factory' ) ) {
 			}
 		}
 
+		/**
+		 * Check if the current message is a question
+		 * @param array $content
+		 * @return bool
+		 * @since 0.0.1
+		 */
 		private function is_question( $content ) {
 			if ( isset( $content[ 'text' ] ) ) {
 				$query		 = '<@U9DQ94KM3> <U9ATTAU00>';
