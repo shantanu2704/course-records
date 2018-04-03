e29a257b44e07e05ab28773fbcfb5bf1c8cddde3
Shantanu <shantanu2846@gmail.com>
2018-04-03 17:10:51 +0530

    Add post date and time

--- components/post/content.php
+++ components/post/content.php
@@ -48,10 +48,11 @@
 	<?php endif; ?>
 
 	<div class="entry-content">
+		<div class="author-avatar"> <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?></div>
+		<span class="author-avatar"> <?php the_author(); ?></span>
+		<span class="post-date"> <?php the_date(); ?></span>
+		<span class="post-time"> <?php the_time(); ?></span>
 		<?php
-
-			echo get_avatar( get_the_author_meta( 'ID' ) , 32 );
-			the_author();
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
