4a9dc0788e64f09eb2226e4f24316b7a95e456ef
Shantanu <shantanu2846@gmail.com>
2018-04-04 14:29:30 +0530

    Revert the changes from content.php

--- components/post/content.php
+++ components/post/content.php
@@ -48,10 +48,6 @@
 	<?php endif; ?>
 
 	<div class="entry-content">
-		<div class="author-avatar"> <?php echo get_avatar( get_the_author_meta( 'ID' ) , 32 ); ?></div>
-		<span class="author-avatar"> <?php the_author(); ?></span>
-		<span class="post-date"> <?php the_date(); ?></span>
-		<span class="post-time"> <?php the_time(); ?></span>
 		<?php
 			/* translators: %s: Name of current post */
 			the_content( sprintf(
