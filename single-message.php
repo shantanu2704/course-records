284b6c23bc8e481212775444ed0574808eed389e
Shantanu <shantanu2846@gmail.com>
2018-04-04 15:30:16 +0530

    Display reactions for messages

--- single-message.php
+++ single-message.php
@@ -9,30 +9,47 @@
  * @since 1.0
  * @version 1.0
  */
-
-get_header(); ?>
+get_header();
+?>
 
 <div class="wrap">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 			<?php
-				/* Start the Loop */
-				while ( have_posts() ) : the_post();
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
 
-					get_template_part( 'components/post/content', 'message' );
+				get_template_part( 'components/post/content', 'message' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+				$reactions = get_post_meta( get_the_ID(), 'cr_reactions' );
+				?>
+				<div class="reactions" data-reactions="
+					 <?php
+					 foreach ( $reactions[ 0 ] as $reaction ) {
+						 foreach ( $reaction[ 'users' ] as $user ) {
+							 echo $user . "\n";
+						 }
+					 }
+					 ?>">
+						 <?php
+						 foreach ( $reactions[ 0 ] as $reaction ) {
+							 echo $reaction[ 'name' ] . " : " . $reaction[ 'count' ];
+						 }
+						 ?>
+				</div>
+				<?php
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-					the_post_navigation( array(
-						'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'previous' ) ) . '</span>%title</span>',
-						'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'next' ) ) . '</span></span>',
-					) );
+				the_post_navigation( array(
+					'prev_text'	 => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'previous' ) ) . '</span>%title</span>',
+					'next_text'	 => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'next' ) ) . '</span></span>',
+				) );
 
-				endwhile; // End of the loop.
+			endwhile; // End of the loop.
 			?>
 
 		</main><!-- #main -->
@@ -40,4 +57,5 @@ get_header(); ?>
 	<?php get_sidebar(); ?>
 </div><!-- .wrap -->
 
-<?php get_footer();
+<?php
+get_footer();
