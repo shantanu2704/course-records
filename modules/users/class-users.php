218633626f6903c3566b72c60a56bb3a3a08b54c
Shantanu <shantanu2846@gmail.com>
2018-04-05 13:29:18 +0530

    Add new class for handling users

---
+++ modules/users/class-users.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Course Records
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) )	exit();
+
+if ( !class_exists( 'Users' ) ) {
+
+	/**
+	 * Slack Input
+	 *
+	 * @since 0.0.1
+	 */
+	class Users {
+
+		/**
+		 * Constructor
+		 */
+		public function __construct() {
+			
+		}
+
+	}
+
+}
