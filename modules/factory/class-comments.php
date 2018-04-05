6a2a9c0a317e3b8d1ca4449334800b568bc68cea
Shantanu <shantanu2846@gmail.com>
2018-04-05 14:29:50 +0530

    Refactor add_comment to user class-users

--- modules/factory/class-comments.php
+++ modules/factory/class-comments.php
@@ -35,8 +35,14 @@ if ( !class_exists( 'Comments' ) ) {
 		
 		public function add_comments() {
 			$ts = (int) $this->content[ 'ts' ];
-			
-			$user = $this->get_username_from_slack_id();
+			$user_id = '';
+			if ( array_key_exists( 'user', $this->content ) ) {
+				$user = $this->content[ 'user' ];
+				
+				require get_parent_theme_file_path( '/modules/users/class-users.php' );
+				$users = new Users();
+				$user_id = $users->get_user_from_slack_id( $user );
+			}
 			
 			// Create post object
 			$my_comment	 = array(
@@ -44,7 +50,7 @@ if ( !class_exists( 'Comments' ) ) {
 				'comment_date'		 => date( "Y-m-d H:i:s", $ts ),
 				'comment_content'	 => $this->content[ 'text' ],
 				'comment_post_ID'	 => $this->parent_id,
-				'user_id'			 => $user[ 0 ]->ID
+				'user_id'			 => $user_id,
 			);
 			// Insert post into database
 			$comment_id =  wp_insert_comment( $my_comment );
