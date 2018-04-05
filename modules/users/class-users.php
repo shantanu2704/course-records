a12c0f26884d8b70a93e9cf75a1c7d9db7bcc050
Shantanu <shantanu2846@gmail.com>
2018-04-05 13:36:59 +0530

    Define two arrays to hold slack IDs and usernames

--- modules/users/class-users.php
+++ modules/users/class-users.php
@@ -17,12 +17,63 @@ if ( !class_exists( 'Users' ) ) {
 	 * @since 0.0.1
 	 */
 	class Users {
+		
+		/**
+		 * An array to hold all the Slack IDs
+		 * @var array
+		 * @since 0.0.1 
+		 */
+		private $slack_user_ids;
+		
+		/**
+		 * An array to hold all usernames
+		 * @var array 
+		 * @since 0.0.1 
+		 */
+		private $usernames;
 
 		/**
 		 * Constructor
 		 */
 		public function __construct() {
 			
+			$this->slack_user_ids = array (
+				'<@U9H7QT561>',	// Archana
+				'<@U9E46DFUH>',	// Chandni
+				'<@U9HUZURK2>',	// Gaurav
+				'<@U9GTQ3J85>',	// Naweed
+				'<@U9K4V3AUE>',	// Jitender
+				'<@U9EFHHDM0>',	// Kamlesh
+				'<@U9E76V412>',	// Parth
+				'<@U9DHNB324>',	// Sasi
+				'<@U9ATTAU00>',	// Saurabh
+				'<@U9HEB6PMX>',	// Sheeba
+				'<@U9EB4N9EH>',	// Tushar
+				'<@U9K0JAMDZ>',	// VIshal
+				'<@U9GMWL93L>',	// Wpshades
+				'<@U9DQ94KM3>',	// must-read
+				'<!channel>',		// channel
+				'<@U9DS10XPG>',	// Akka
+			);
+			
+			$this->usernames = array(
+				'@Archana',
+				'@Chandni',
+				'@Gaurav',
+				'@Naweed',
+				'@Jitender',
+				'@Kamlesh',
+				'@Parth',
+				'@Sasi',
+				'@Saurabh',
+				'@Sheeba',
+				'@Tushar',
+				'@VIshal',
+				'@Wpshades',
+				'@must-read',
+				'@channel',
+				'@Akka',
+			);
 		}
 
 	}
