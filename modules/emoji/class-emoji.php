b38676c16e2613d153d2f0bc127a38ba901d80b0
Shantanu <shantanu2846@gmail.com>
2018-04-10 14:50:40 +0530

    Add a method to enqueue scripts and styles

--- modules/emoji/class-emoji.php
+++ modules/emoji/class-emoji.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Course Records
  * 
@@ -6,7 +7,7 @@
  * @since 0.0.1
  */
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) exit();
+if ( !defined( 'ABSPATH' ) )	exit();
 
 if ( !class_exists( 'Emoji' ) ) {
 
@@ -16,7 +17,7 @@ if ( !class_exists( 'Emoji' ) ) {
 	 * @since 0.0.1
 	 */
 	class Emoji {
-		
+
 		/**
 		 * Initialse this class
 		 * 
@@ -27,5 +28,27 @@ if ( !class_exists( 'Emoji' ) ) {
 			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_emoji_styles_and_scripts' ) );
 		}
 
+		/**
+		 * Enqueue the styles and scripts for emojis
+		 * Library - https://github.com/emojione/emojify.js
+		 * @since 0.0.1
+		 */
+		public function enqueue_emoji_styles_and_scripts() {
+			$style_src = '//cdnjs.cloudflare.com/ajax/libs/emojify.js/1.1.0/css/basic/emojify.min.css';
+			if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+				$style_src = '//cdnjs.cloudflare.com/ajax/libs/emojify.js/1.1.0/css/basic/emojify.css';
+			}
+
+			$script_src = '//cdnjs.cloudflare.com/ajax/libs/emojify.js/1.1.0/js/emojify.min.js';
+			if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
+				$style_src = '//cdnjs.cloudflare.com/ajax/libs/emojify.js/1.1.0/js/emojify.js';
+			}
+
+			wp_enqueue_style( 'emoji_style', $style_src );
+			wp_enqueue_script( 'emoji_script', $script_src );
+			wp_enqueue_script( 'cr_emoji', get_parent_theme_file_path( '/assets/js/cr-emoji.js' ), array( 'jquery', 'emoji_script' ) );
+		}
+
 	}
+
 }
