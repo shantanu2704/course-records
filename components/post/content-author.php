1c899cb834654b052e855d7607fbf106afc96ecf
Shantanu <shantanu2846@gmail.com>
2018-04-10 15:49:31 +0530

    Add a new template file to include the post-author details

---
+++ components/post/content-author.php
@@ -0,0 +1,4 @@
+<div class="author-avatar"> <?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></div>
+<span class="author-avatar"> <?php the_author(); ?></span>
+<span class="post-date"> <?php the_date(); ?></span>
+<span class="post-time"> <?php the_time(); ?></span>
