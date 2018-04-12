d12c3d80ec22a0cb18a65f68ab2f053e6465ed21
Shantanu <shantanu2846@gmail.com>
2018-04-12 14:06:07 +0530

    Add a new class Reactions

---
+++ modules/emoji/class-reactions.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Course Records
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) )	exit();
+
+if ( !class_exists( 'Reactions' ) ) {
+
+	/**
+	 * Reactions
+	 *
+	 * @since 0.0.1
+	 */
+	class Reactions { 
+		
+	}
+}
