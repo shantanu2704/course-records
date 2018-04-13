cfce7e9dc16a93f88ae78c32ea34038409b3e96b
Shantanu <shantanu2846@gmail.com>
2018-04-13 16:15:27 +0530

    Add checks for posts with no reactions

--- components/course-records/reaction.php
+++ components/course-records/reaction.php
@@ -4,10 +4,12 @@
  * @since 0.0.1
  */
 require get_parent_theme_file_path( '/inc/template-utilities.php' );
-$user_list_for_other_reactions	 = get_user_list_for_other_reactions();
+$user_list_for_other_reactions = get_user_list_for_other_reactions();
 ?>
 <div class="cr-reactions" id="cr-reactions" >
-	<ul class="cr-post-reaction" id="cr-post-reaction"> <?php get_reactions_on_post(); ?> </ul>
-	<ul class="cr-other-reactions" id="cr-other-reactions"> Reactions: <?php user_list_for_other_reactions( $user_list_for_other_reactions ); ?> </ul>
+	<ul class="cr-post-reaction" id="cr-post-reaction"> Reactions: <?php get_reactions_on_post(); ?> </ul>
+	<?php if ( !empty( $user_list_for_other_reactions ) ) : ?> 
+		<ul class="cr-other-reactions" id="cr-other-reactions"> <?php user_list_for_other_reactions( $user_list_for_other_reactions ); ?> </ul>
+	<?php endif; ?>
 </div>
 
