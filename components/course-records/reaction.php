4881e02cb015434dc5bc8e7045fa8126cdb86084
Shantanu <shantanu2846@gmail.com>
2018-04-13 14:34:20 +0530

    Add headings to lists

--- components/course-records/reaction.php
+++ components/course-records/reaction.php
@@ -9,8 +9,8 @@ $user_list_for_other_reactions	 = get_user_list_for_other_reactions();
 ?>
 <div class="cr-reactions" id="cr-reactions" >
 	<ul class="cr-post-reaction" id="cr-post-reaction"> <?php get_reactions_on_post(); ?> </ul>
-	<ul class="cr-complete-task" id="cr-complete-task"> <?php user_list_complete_task( $user_list_for_checkmarks[ 'complete' ] ); ?> </ul>
-	<ul class="cr-incomplete-task" id="cr-incomplete-task"> <?php user_list_complete_task( $user_list_for_checkmarks[ 'incomplete' ] ); ?> </ul>
-	<ul class="cr-other-reactions" id="cr-other-reactions"> <?php user_list_for_other_reactions( $user_list_for_other_reactions ); ?> </ul>
+	<ul class="cr-complete-task" id="cr-complete-task"> Users who have completed the task: <?php user_list_complete_task( $user_list_for_checkmarks[ 'complete' ] ); ?> </ul>
+	<ul class="cr-incomplete-task" id="cr-incomplete-task"> Users who haven't completed the task:<?php user_list_complete_task( $user_list_for_checkmarks[ 'incomplete' ] ); ?> </ul>
+	<ul class="cr-other-reactions" id="cr-other-reactions"> Reactions: <?php user_list_for_other_reactions( $user_list_for_other_reactions ); ?> </ul>
 </div>
 
