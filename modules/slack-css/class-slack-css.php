5359eb0f4432e31175ca22a9a730e460c7ad4277
Shantanu <shantanu2846@gmail.com>
2018-04-18 14:54:03 +0530

    Fix the stylesheet path

--- modules/slack-css/class-slack-css.php
+++ modules/slack-css/class-slack-css.php
@@ -28,7 +28,7 @@ if ( !class_exists( 'Slack_CSS' ) ) {
 		}
 		
 		public function slack_style() {
-			$style_src = get_parent_theme_file_uri() . 'sass/slack-style.css';
+			$style_src = get_parent_theme_file_uri( 'slack-style.css' );
 			wp_enqueue_style( 'slack_style', $style_src );
 		}
 
