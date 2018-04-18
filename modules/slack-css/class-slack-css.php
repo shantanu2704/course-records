cfccd08311e214ae984b1e9a8a6a347f2dca8234
Shantanu <shantanu2846@gmail.com>
2018-04-18 13:15:53 +0530

    Add a method to enqueue the slack stylesheet

--- modules/slack-css/class-slack-css.php
+++ modules/slack-css/class-slack-css.php
@@ -26,6 +26,12 @@ if ( !class_exists( 'Slack_CSS' ) ) {
 
 			add_action( 'wp_enqueue_scripts', array( $this, 'slack_style' ) );
 		}
+		
+		public function slack_style() {
+			$style_src = get_parent_theme_file_uri() . 'sass/slack-style.scss';
+			wp_enqueue_style( 'slack_style', $style_src );
+		}
+
 	}
 
 }
