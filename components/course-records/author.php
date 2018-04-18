f5d09b9e566477c57508aa87e76f57375ab675ac
Shantanu <shantanu2846@gmail.com>
2018-04-18 13:23:30 +0530

    Add the theme prefix to the author gravatar and author name

--- components/course-records/author.php
+++ components/course-records/author.php
@@ -1,2 +1,2 @@
-<div class="author-avatar"> <?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?></div>
-<span class="author-avatar"> <?php the_author(); ?></span>
+<div class="cr-author-avatar"> <?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?></div>
+<span class="cr-author-name"> <?php the_author(); ?></span>
