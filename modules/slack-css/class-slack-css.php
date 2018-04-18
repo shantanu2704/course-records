61dafa037f4c04d4061b049dbd3867f28f5dc605
Shantanu <shantanu2846@gmail.com>
2018-04-18 13:10:35 +0530

    Add a new class to enqueue the css for Slack

---
+++ modules/slack-css/class-slack-css.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Course Records
+ * 
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1
+ */
+// If this file is called directly, abort.
+if ( !defined( 'ABSPATH' ) ) exit();
+
+if ( !class_exists( 'Slack_CSS' ) ) {
+
+	/**
+	 * Slack CSS
+	 *
+	 * @since 0.0.1
+	 */
+	class Slack_CSS {
+		
+	}
+
+}
