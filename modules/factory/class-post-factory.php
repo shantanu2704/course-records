da38dd493afbe3c70a60f12820139350548c0d2b
Shantanu <shantanu2846@gmail.com>
2018-04-21 13:02:56 +0530

    Fix cs issues in class-post-factory

--- modules/factory/class-post-factory.php
+++ modules/factory/class-post-factory.php
@@ -1,15 +1,18 @@
 <?php
-
 /**
  * Course Records
- * 
+ *
  * @author Shantanu Desai <shantanu2846@gmail.com>
  * @since 0.0.1
+ * @package course-records
+ * 
  */
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) ) exit();
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
 
-if ( !class_exists( 'Post_Factory' ) ) {
+if ( ! class_exists( 'Post_Factory' ) ) {
 
 	/**
 	 * Post Factory
@@ -17,21 +20,12 @@ if ( !class_exists( 'Post_Factory' ) ) {
 	 * @since 0.0.1
 	 */
 	class Post_Factory {
-		
-		private $content;
-		
-		/**
-		 * Constructor
-		 * @param array $json_content Decoded json data
-		 */
-		public function __construct( ) {
-//			$this->content = array();
-//			$this->content = $json_content;
-		}
 
 		/**
 		 * Instantiate the appropriate class
-		 * @param array $post_type JSON of a message converted to an array
+		 * @param array $content JSON of a message converted to an array.
+		 * @param array $post_type Post type.
+		 * @param array $post_id Post ID.
 		 * @since 0.0.1
 		 */
 		public function instantiate_classes( $content, $post_type, $post_id = 0 ) {
@@ -41,17 +35,17 @@ if ( !class_exists( 'Post_Factory' ) ) {
 			require get_parent_theme_file_path( '/modules/factory/class-comments.php' );
 			
 
-			if ( $post_type === 'Task' ) {
-				$task = new Tasks( $content ); // Create new Task
+			if ( 'Task' === $post_type ) {
+				$task = new Tasks( $content ); // Create new Task.
 				return $task->add_task();
-			}elseif ( $post_type === 'Question' ) {
-				$question = new Questions( $content ); // Create new Question
+			} elseif ( 'Question' === $post_type ) {
+				$question = new Questions( $content ); // Create new Question.
 				return $question->add_question();
-			}elseif ( ( $post_type === 'First Thread Message' ) || ( $post_type === 'Message' ) ){
-				$message = new Messages( $content ); // Create new Message		
+			} elseif ( ( 'First Thread Message' === $post_type ) || ( 'Message' === $post_type ) ) {
+				$message = new Messages( $content ); // Create new Message.		
 				return $message->add_message();
-			}elseif ( $post_type === 'Comment' ) {
-				$message = new Comments( $content, $post_id ); // Create new comment
+			} elseif ( 'Comment' === $post_type ) {
+				$message = new Comments( $content, $post_id ); // Create new comment.
 				$message->add_comments();
 				return $post_id;
 			}
