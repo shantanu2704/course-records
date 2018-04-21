4e7568a162da946af42b60452c321db72ec14029
Shantanu <shantanu2846@gmail.com>
2018-04-21 13:27:13 +0530

    Fix cs issues in class-reactions

--- modules/emoji/class-reactions.php
+++ modules/emoji/class-reactions.php
@@ -1,18 +1,21 @@
 <?php
-
 /**
  * Course Records
- * 
+ *
  * @author Shantanu Desai <shantanu2846@gmail.com>
  * @since 0.0.1
+ * @package 0.0.1
  */
+
 // If this file is called directly, abort.
-if ( !defined( 'ABSPATH' ) )	exit();
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
 
 require get_parent_theme_file_path( '/modules/users/class-users.php' );
 
-if ( !class_exists( 'Reactions' ) ) {
-	
+if ( ! class_exists( 'Reactions' ) ) {
+
 	/**
 	 * Reactions
 	 *
@@ -21,21 +24,25 @@ if ( !class_exists( 'Reactions' ) ) {
 	class Reactions {
 
 		/**
+		 * Array to store reactions on a post 
 		 *
-		 * @var array Array to store reactions on a post 
+		 * @var array
+		 * @since 0.0.1
 		 */
 		private $reactions;
-		
+
 		/**
 		 * Object of type Users
-		 * @var object  
+		 * 
+		 * @var object
+		 * @since 0.0.1
 		 */
 		private $users;
 
 
 		/**
 		 * Constructor
-		 * 
+		 *
 		 * @since 0.0.1
 		 */
 		public function __construct() {
@@ -45,30 +52,36 @@ if ( !class_exists( 'Reactions' ) ) {
 		
 		/**
 		 * Getter for reactions
+		 *
 		 * @return array
+		 * @since 0.0.1
 		 */
 		public function get_reactions() {
 			return $this->reactions;
 		}
-		
+
 		/**
 		 * Gets the list of reactions and their counts on a post
+		 * 
 		 * @return array
+		 * @since 0.0.1
 		 */
 		public function get_reactions_on_post() {
-			return array_column( $this->reactions, 'name', 'count' ); //Gets the list of reactions and their counts as key-value pairs
+			return array_column( $this->reactions, 'name', 'count' ); //Gets the list of reactions and their counts as key-value pairs.
 		}
 		
 		/**
 		 * Get a multidimensional array of reactions and the users who have reacted
+		 * 
 		 * @return array
+		 * @since 0.0.1
 		 */
 		public function get_userlist_with_reactions() {
-			$reactors = array ();
-			if ( ! empty ($this->reactions ) ) {
+			$reactors = array();
+			if ( ! empty( $this->reactions ) ) {
 				foreach ( $this->reactions as $reaction ) {
-					if ( $reaction[ 'name' ] != 'white_check_mark' ) {
-						$reactors[ $reaction[ 'name' ] ] = $this->users->get_names_from_slack_ids( $reaction[ 'users' ] );
+					if ( 'white_check_mark' !== $reaction['name'] ) {
+						$reactors[ $reaction['name'] ] = $this->users->get_names_from_slack_ids( $reaction['users'] );
 					}
 				}
 			}
