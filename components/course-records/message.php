3c46639dd4859390e53d6e002207214cfef3db10
Shantanu <shantanu2846@gmail.com>
2018-04-23 13:44:36 +0530

    Fix cs issues in message

--- components/course-records/message.php
+++ components/course-records/message.php
@@ -50,8 +50,8 @@
 
 	<div class="entry-content">
 		<?php
-			/* translators: %s: Name of current post */
 			the_content( sprintf(
+				/* translators: %s: Name of current post */
 				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
 				get_the_title()
 			) );
