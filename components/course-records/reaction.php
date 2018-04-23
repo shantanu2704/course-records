01e9f46dd24718c678d9e3947b5e23ed5839a441
Shantanu <shantanu2846@gmail.com>
2018-04-23 13:53:50 +0530

    Fix cs issues in reaction

--- components/course-records/reaction.php
+++ components/course-records/reaction.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * Displays user reactions
- * @since 0.0.1
+ *
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1 
+ * @package course-records
  */
+
 require get_parent_theme_file_path( '/inc/template-utilities.php' );
 $user_list_for_other_reactions = get_user_list_for_other_reactions();
 ?>
 <div class="cr-reactions" id="cr-reactions" >
 	<ul class="cr-post-reaction" id="cr-post-reaction"> Reactions: <?php get_reactions_on_post(); ?> </ul>
-	<?php if ( !empty( $user_list_for_other_reactions ) ) : ?> 
+	<?php if ( ! empty( $user_list_for_other_reactions ) ) : ?> 
 		<ul class="cr-other-reactions" id="cr-other-reactions"> <?php user_list_for_other_reactions( $user_list_for_other_reactions ); ?> </ul>
 	<?php endif; ?>
 </div>
