d97ea39ad88e2c7e1217eb6c6aa57e583aed42e1
Shantanu <shantanu2846@gmail.com>
2018-04-23 14:03:10 +0530

    Fix cs issues in single-task

--- single-task.php
+++ single-task.php
@@ -2,13 +2,11 @@
 /**
  * The template for displaying all single posts
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
+ * @author Shantanu Desai <shantanu2846@gmail.com>
+ * @since 0.0.1 
+ * @package course-records
  */
+
 get_header();
 ?>
 
@@ -18,19 +16,20 @@ get_header();
 
 			<?php
 			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			while ( have_posts() ) :
+				the_post();
 
 				get_template_part( 'components/course-records/task' );
 				get_template_part( 'components/course-records/reaction', 'task' );
-				
+
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 
 				the_post_navigation( array(
-					'prev_text'	 => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'previous' ) ) . '</span>%title</span>',
-					'next_text'	 => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'next' ) ) . '</span></span>',
+					'prev_text'  => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'previous' ) ) . '</span>%title</span>',
+					'next_text'  => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'next' ) ) . '</span></span>',
 				) );
 
 			endwhile; // End of the loop.
